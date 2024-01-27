741c37c21b59759019d37867776df9bf1416d62a
niblicat <maybenaebli@gmail.com>
2024-01-26 22:04:10 -0600

    made stringtolower function

--- main.cpp
+++ main.cpp
@@ -3,13 +3,18 @@
 #include <algorithm>
 using namespace std;
 
+void StringToLower(string& given) {
+    std::transform(given.begin(), given.end(), given.begin(),
+        [](unsigned char c){ return std::tolower(c); });
+}
+
 int main() {
     string userNoise;
     cout << "What does a goose say? ";
     cin >> userNoise;
-    std::transform(userNoise.begin(), userNoise.end(), userNoise.begin(),
-        [](unsigned char c){ return std::tolower(c); });
-
+    
+    StringToLower(userNoise);
+    
     while (userNoise != "honk") {
         cout << "You are absolutely incorrect. Please try again.\n";
 
