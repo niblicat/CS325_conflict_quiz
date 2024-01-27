f2974cc2529f8e43c30f324694c66490f7d53d8d
Jimmy <hungtran2003ti@gmail.com>
2024-01-26 22:38:00 -0600

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 #include <algorithm>
 using namespace std;
 
+void StringToLower(string& given) {
+    std::transform(given.begin(), given.end(), given.begin(),
+        [](unsigned char c){ return std::tolower(c); });
+}
+
 int main() {
     string userNoise;
     string lowerUserNoise;
